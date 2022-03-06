a9cc6c19330edf6106b5086169f387365ed04082
RPi4 <b.lee@utexas.edu>
2022-03-06 17:44:44 +0900

    removed unnecessary import in I2C LCD example

--- single_components/Example_I2C_LCD.py
+++ single_components/Example_I2C_LCD.py
@@ -1,8 +1,6 @@
 import I2C_LCD_driver
-from time import *
 
 mylcd = I2C_LCD_driver.LCD()
 
-# mylcd.lcd_display_string("1234567890123456", 1) # Allows up to 16 characters per line
 mylcd.lcd_display_string("Neither Smart,", 1)
 mylcd.lcd_display_string("nor a Farm. Yet.", 2)
